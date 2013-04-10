b3506daf14fda5496b4073b83c484ac4b2d4f653
lmorandini <lmorandini@ieee.org>
2013-04-10 17:50:44 +1000

    Revised analysis, added Selenium test script to execute tests (12 tests of repeated tests, just to hammer out fluctuations)

--- test/result/analysis.R
+++ test/result/analysis.R
@@ -1,7 +1,19 @@
 setwd("/usr/var/projects/aurin/git/map-rendering-test/test/result")
 pbc.df<-read.table("pbc.csv",header=T, sep=",")
+
 pbc.mod=PointsPerSec~Size+Npoints+Type+Precision+Dataset+Generalization+Compression+Npoints+Ngeoms
-pairs(pbc.mod, pbc.df)
+pbc.modP=Npoints~Generalization+Ngeoms
+pbc.modS=Size~Precision+Compression+Npoints+Ngeoms
+pbc.modT=PointsPerSec~Size+Npoints
 
 
+# pairs(pbc.modP, pbc.df)
+#plot(pbc.df$Npoints[pbc.df$Npoints<10000], pbc.df$PointsPerSec[pbc.df$Npoints<10000])
+#plot(pbc.df$Size, pbc.df$PointsPerSec)
+#plot(pbc.df$Npoints, pbc.df$Time)
+#plot(pbc.df$Size, pbc.df$Time)
+plot(pbc.df$Npoints[pbc.df$Npoints<2000], pbc.df$PointsPerSec[pbc.df$Npoints<2000])
+hist(pbc.df$Npoints)
+avg(pbc.df$Npoints)
+hist(pbc.df$Npoints[pbc.df$Npoints<10000])
 
